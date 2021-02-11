56c3629075be41623720e23042bd87795577e6c9
JWKennington <jameswkennington@gmail.com>
2021-02-11 01:33:07 -0500

    add initial script based on Creute Z2 code

---
+++ z2_example.h
@@ -0,0 +1,14 @@
+//
+// Created by Jim on 2/8/21.
+//
+
+#ifndef CGFTTEST_Z2_EXAMPLE_H
+#define CGFTTEST_Z2_EXAMPLE_H
+
+#endif //CGFTTEST_Z2_EXAMPLE_H
+
+//int link;
+void moveup(int x[],int d);
+void movedown(int x[],int d);
+void coldstart();
+double update(double beta);
